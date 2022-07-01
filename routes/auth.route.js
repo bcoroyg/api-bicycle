d96981020e0d182cceea5182c0172c0176ea6f90
Emilio Coroy <becgonzalez25@gmail.com>
2022-06-30 18:48:31 -0600

    Documentación de login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,29 @@ import authController from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.get('/confirmation/:token', authController.getConfirmationAccount);
+
+/**
+ * Login user
+ * @openapi
+ * /login:
+ *    post:
+ *      tags: [Auth]
+ *      summary: "Login user"
+ *      description: login and return token
+ *      requestBody:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                 type: object
+ *                 $ref: "#/components/schemas/authLogin"
+ *      responses:
+ *        '200':
+ *          description: Return token.
+ *        '401':
+ *          description: User or password incorrect.
+ */
 router.post('/login', authController.login);
+
 router.post('/forgot-password', authController.postForgotPassword);
 router.get('/reset-password/:token', authController.getResetPassword);
 router.post('/reset-password', authController.postResetPassword);
